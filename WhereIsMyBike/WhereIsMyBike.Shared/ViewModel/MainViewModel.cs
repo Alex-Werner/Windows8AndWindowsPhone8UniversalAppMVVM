03972eb9f90a6d49d2709658e6d156a067715874
Alex-Werner <obusco@gmail.com>
2014-10-23 16:28:06 +0200

    Initialisation du projet

---
+++ WhereIsMyBike/WhereIsMyBike.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Threading.Tasks;
+
+namespace WhereIsMyBike.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get
+            {
+                return _helloWorld;
+            }
+            set
+            {
+                Set(() => HelloWorld, ref _helloWorld, value);
+            }
+        }
+        
+        public MainViewModel()
+        {
+            HelloWorld = IsInDesignMode
+             ? "Runs in design mode"
+             : "Runs in runtime mode";
+        }
+    
+    }
+}
---
+++ WhereIsMyBike/WhereIsMyBike.Shared/ViewModel/StationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Threading.Tasks;
+
+namespace WhereIsMyBike.ViewModel
+{
+    public class StationViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get
+            {
+                return _helloWorld;
+            }
+            set
+            {
+                Set(() => HelloWorld, ref _helloWorld, value);
+            }
+        }
+
+        public StationViewModel()
+        {
+            HelloWorld = IsInDesignMode
+             ? "Runs in design mode _ Station"
+             : "Runs in runtime mode _ Station";
+        }
+    }
+}
