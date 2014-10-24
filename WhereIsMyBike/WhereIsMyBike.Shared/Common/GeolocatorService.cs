97514273f48cce9ca5cdb546d68b10348a7fb827
Alex-Werner <obusco@gmail.com>
2014-10-24 10:32:16 +0200

    Added automatic localisation on start

---
+++ WhereIsMyBike/WhereIsMyBike.Shared/Common/GeolocatorService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace WhereIsMyBike.Common
+{
+    public class GeolocatorService
+    {
+        public async Task<Geoposition> GetGeoposition()
+        {
+            var loc = new Geolocator();
+            return await loc.GetGeopositionAsync();
+        }
+    }
+}
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using System.Net.Http;
+using System.IO;
+using Windows.Devices.Geolocation;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace WhereIsMyBike.Common
+{
+    public sealed class GeolocatorService : IBackgroundTask
+    {
+
+        public Geoposition currentPosition { get; set; }
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            currentPosition = await GetPosition();
+            deferral.Complete();
+        }
+
+        public Windows.Foundation.IAsyncOperation<Geoposition> GetPositionAsync()
+        {
+            return AsyncInfo.Run((Geoposition ge) => GetPosition());
+
+        }
+        public async Task<Geoposition> GetPosition()
+        {
+            Geoposition geoposition = null;
+
+            Geolocator geolocator = new Geolocator();
+
+            geolocator.DesiredAccuracyInMeters = 50;
+
+            try
+            {
+                geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10)
+                    );
+
+            }
+            catch (ArgumentException AE)
+            {
+                throw AE;
+            }
+
+            return geoposition;
+        }
+
+    }
+}
+*/
--- WhereIsMyBike/WhereIsMyBike.Windows/View/MainPage.xaml.cs
+++ WhereIsMyBike/WhereIsMyBike.Windows/View/MainPage.xaml.cs
@@ -10,6 +10,15 @@ using Windows.Foundation.Collections;
 using Bing.Maps;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using WhereIsMyBike.Common;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace WhereIsMyBike.View
@@ -19,13 +28,68 @@ namespace WhereIsMyBike.View
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
+        
         public MainPage()
         {
+
             this.InitializeComponent();
+           InitializeMap();
+
+
+
+            //_geolocator = new Geolocator();
+            //_geolocator.ReportInterval = 1000; //1sec
+
+            //_geolocator.PositionChanged += new TypedEventHandler<Geolocator, PositionChangedEventArgs>(OnPositionChanged);
+           // _geolocator.StatusChanged += new TypedEventHandler<Geolocator, StatusChangedEventArgs>(OnStatusChanged);
+           // Geoposition pos = e.Position;
+
+
             
         }
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+       /* public async void GetPosition()
         {
+            // Get my current location.
+            Geolocator myGeolocator = new Geolocator();         
+            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
+            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
+           // GeoCoordinate myGeoCoordinate = CoordinateConverter.ConvertGeocoordinate(myGeocoordinate);
+
+            // Make my current location the center of the Map.
+            // this.myMap.Center = myGeoCoordinate;
+            this.myMap.ZoomLevel = 13;
+
+          
+        }*/
+       
+        void InitializeMap()
+        {
+
+            myMap.Center = new Location(43.596931999999995,1.421816);
+            myMap.ZoomLevel = 12;
+            myMap.MapType = MapType.Road;
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Debug.WriteLine("hey je suis onNavigateTO");
+            var geoLocator = new GeolocatorService();
+            try
+            {
+                Debug.WriteLine("hey je suis try");
+
+                var t = await geoLocator.GetGeoposition();
+                var longitude = t.Coordinate.Longitude;
+                var latitude = t.Coordinate.Latitude;
+                myMap.Center = new Location(latitude, longitude);
+                myMap.ZoomLevel = 15;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("hey je suis AIE");
+
+            }
         }
     }
 }
